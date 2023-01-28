00334fc91c647a8ebd5678208716b15c747c4b89
Vitor Nere <vitor.nere@hotmail.com>
2023-01-28 15:09:49 -0300

    Adding cors headers

--- enpyre_play/settings/application.py
+++ enpyre_play/settings/application.py
@@ -8,6 +8,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'social_django',
     'rest_social_auth',
     'enpyre_play.users',
@@ -16,6 +17,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
