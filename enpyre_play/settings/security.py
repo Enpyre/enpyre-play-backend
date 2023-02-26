ca1331a77694c77baaa4f072f845c7b548f2f08b
Vitor Nere <vitor.nere@hotmail.com>
2023-02-26 18:14:36 -0300

    Allowing production dns from cors

--- enpyre_play/settings/security.py
+++ enpyre_play/settings/security.py
@@ -1,15 +1,24 @@
 # from enpyre_play.enums import EnvironmentSet
-from enpyre_play.envs import JWT_SECRET_KEY  # , ENVIRONMENT
+from enpyre_play.enums import EnvironmentSet
+from enpyre_play.envs import ENVIRONMENT, JWT_SECRET_KEY
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = JWT_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # ENVIRONMENT == EnvironmentSet.DEVELOPMENT
+DEBUG = ENVIRONMENT == EnvironmentSet.DEVELOPMENT
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://enpyre-play.vercel.app',
+]
 
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000']
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://enpyre-play.vercel.app',
+]
