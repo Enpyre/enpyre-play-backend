2b741667203892bc2d23949af4aeac921fd4a6e1
Lucas Amoêdo <lucas.advc@gmail.com>
2023-03-26 18:43:03 -0300

    Update project code column

--- enpyre_play/projects/models.py
+++ enpyre_play/projects/models.py
@@ -8,7 +8,7 @@ from enpyre_play.users.utils import get_sentinel_user
 class Project(BaseModel):
     title = models.CharField(max_length=100, help_text='Title of the project')
     description = models.TextField(null=True, blank=True, help_text='Description of the project')
-    code = models.JSONField(null=True, blank=False, help_text='Code of the project')
+    code = models.TextField(null=True, blank=False, help_text='Code of the project')
     link = models.URLField(null=True, blank=True, unique=True, help_text='Link to the project')
     shared = models.BooleanField(default=False, help_text='Is the project shared?')
     public = models.BooleanField(default=False, help_text='Is the project public?')
@@ -18,3 +18,6 @@ class Project(BaseModel):
         on_delete=models.SET(get_sentinel_user),
         related_name='projects',
     )
+
+    def __str__(self):
+        return self.title
--- enpyre_play/users/models.py
+++ enpyre_play/users/models.py
@@ -2,5 +2,5 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+class User(AbstractUser):  # type: ignore
     picture = models.URLField(blank=True, null=True)
