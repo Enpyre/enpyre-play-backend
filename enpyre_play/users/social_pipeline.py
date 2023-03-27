9fd9ee815c1afaebbd6b8dddffedacb3a6db38eb
Vitor Nere <vitor.nere@hotmail.com>
2023-03-26 21:41:34 -0300

    Save picture with avatar only

--- enpyre_play/users/social_pipeline.py
+++ enpyre_play/users/social_pipeline.py
@@ -6,19 +6,13 @@ def auto_logout(*args, **kwargs):
     return {'user': None}
 
 
-def save_avatar(strategy, details, *args, user=None, **kwargs):
+def save_avatar(strategy, *args, user=None, **kwargs):
     """Get user avatar from social provider."""
     if user:
-        backend_name = kwargs['backend'].__class__.__name__.lower()
-        response = kwargs.get('response', {})
-        picture = None
-        if 'github' in backend_name and response.get('avatar_url'):
-            picture = response['avatar_url']
-        else:
-            picture = 'https://api.dicebear.com/5.x/bottts-neutral/svg?'
-            picture += f'seed={user.username}'
-            picture += '&size=100&backgroundType=gradientLinear,solid'
-        if picture and user.picture != picture:
+        picture = 'https://api.dicebear.com/5.x/bottts-neutral/svg?'
+        picture += f'seed={user.username}'
+        picture += '&size=100&backgroundType=gradientLinear,solid'
+        if user.picture != picture:
             user.picture = picture
             strategy.storage.user.changed(user)
 
