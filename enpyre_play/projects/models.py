320b9068a9e7ce28e1b93058056b502b196d572e
Lucas Amoêdo <lucas.advc@gmail.com>
2023-04-04 23:00:31 -0300

    allow code to be blank

--- enpyre_play/projects/models.py
+++ enpyre_play/projects/models.py
@@ -8,7 +8,7 @@ from enpyre_play.users.utils import get_sentinel_user
 class Project(BaseModel):
     title = models.CharField(max_length=100, help_text='Title of the project')
     description = models.TextField(null=True, blank=True, help_text='Description of the project')
-    code = models.TextField(null=True, blank=False, help_text='Code of the project')
+    code = models.TextField(null=True, blank=True, help_text='Code of the project')
     link = models.URLField(null=True, blank=True, unique=True, help_text='Link to the project')
     shared = models.BooleanField(default=False, help_text='Is the project shared?')
     public = models.BooleanField(default=False, help_text='Is the project public?')
@@ -25,7 +25,7 @@ class Project(BaseModel):
 
 class ProjectSolution(BaseModel):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='solutions')
-    code = models.TextField(null=True, blank=False, help_text='Solution for the project')
+    code = models.TextField(null=True, blank=True, help_text='Solution for the project')
     user = models.ForeignKey(
         User,
         on_delete=models.SET(get_sentinel_user),
